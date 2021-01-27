664ae733d2b665d749bad6a78e363b10633b42fa
Jordan Vlieg <jordanvlieg@gmail.com>
2021-01-26 19:02:18 -0800

    Switch to hashrouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import resume from '../assets/JVlieg_Resume.pdf'
 import Home from './Home'
 
 class App extends Component {
   render() {
     return (      
-       <Router basename={process.env.PUBLIC_URL}>
+       <Router>
         <div>
           <Switch>
             <Route path="/" component={Home} exact/>
