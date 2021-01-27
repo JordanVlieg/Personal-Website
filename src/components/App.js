6445764fd50de78d6aba6139d3bd19f3bb01a2aa
Jordan Vlieg <jordanvlieg@gmail.com>
2021-01-26 17:25:49 -0800

    Attempt to fix routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import resume from '../assets/JVlieg_Resume.pdf'
 import Home from './Home'
 
 class App extends Component {
   render() {
     return (      
-       <BrowserRouter>
+       <Router basename={process.env.PUBLIC_URL}>
         <div>
           <Switch>
             <Route path="/" component={Home} exact/>
             <Route path="/resume" component={Resume}/>
           </Switch>
         </div> 
-      </BrowserRouter>
+      </Router>
     );
   }
 }
